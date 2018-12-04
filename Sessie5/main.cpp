81f4deac3cc7816223332403c7c03781a4b53b3a
laurensvdwalle <laurens.vandewalle@student.kuleuven.be>
2018-12-04 21:57:22 +0100

    Sessie 5: trainingdata en labels toegevoegd, KNN classifier aanmaken lukt niet

--- Sessie5/main.cpp
+++ Sessie5/main.cpp
@@ -83,6 +83,8 @@ int main(int argc, const char** argv)
     imshow(window_name, img);
     waitKey(0);
 
+    // descriptor bepalen op basis van de punten in HSV
+
     return 0;
 }
 
@@ -127,5 +129,48 @@ static void onMouse(int event, int x, int y, int flags, void* userdata)
             cout << "Punt op (x=" << punten[welkePunten][punt].x << " y=" << punten[welkePunten][punt].y << ")" << endl;
         }
     }
+}
+
+void trainKNN(Mat img)
+{
+    int K = 3;
+
+    // image converteren naar hsv
+    Mat hsvImage;
+    cvtColor(img,hsvImage, CV_BGR2HSV);
+
+    // goede waarden inlezen
+    Mat trainingDataGoed(punten[GOED].size(), 3, CV_32FC1);     // trainingdata, met 3 waarden: h, s en v
+    Mat labelsGoed(punten[GOED].size(), 1, CV_32SC1);           // labels, met 1 waarden: GOED of SLECHT
+
+    for( int i=0 ; i<punten[GOED].size() ; i++)
+    {
+        Vec3b descriptor = hsvImage.at<Vec3b>(punten[GOED][i].y, punten[GOED][i].x);
+        trainingDataGoed.at<float>(i,0) = descriptor[0];
+        trainingDataGoed.at<float>(i,1) = descriptor[1];
+        trainingDataGoed.at<float>(i,2) = descriptor[2];
+        labelsGoed.at<float>(i) = GOED;
+    }
+
+    // slecht waarden inlezen
+    Mat trainingDataSlecht(punten[SLECHT].size(), 3, CV_32FC1); // trainingdata, met 3 waarden: h, s en v
+    Mat labelsSlecht(punten[SLECHT].size(), 1, CV_32SC1);       // labels, met 1 waarden: GOED of SLECHT
+
+    for( int i=0 ; i<punten[SLECHT].size() ; i++)
+    {
+        Vec3b descriptor = hsvImage.at<Vec3b>(punten[SLECHT][i].y, punten[SLECHT][i].x);
+        trainingDataSlecht.at<float>(i,0) = descriptor[0];
+        trainingDataSlecht.at<float>(i,1) = descriptor[1];
+        trainingDataSlecht.at<float>(i,2) = descriptor[2];
+        labelsSlecht.at<float>(i) = SLECHT;
+    }
+
+    // samenvoegen goede en slechte trainingdata
+    Mat trainingData, labels;
+    vconcat(trainingDataGoed, trainingDataSlecht, trainingData);
+    vconcat(labelsGoed, labelsSlecht, labels);
+
+    // CvKNearest knn( trainingData, labels, 0, false, K );
+    // CvMat* nearests = cvCreateMat( 1, K, CV_32FC1);
 }
 //--i1=/home/laurens/Documents/Laurens/2018_beeldinterpretatie_VandeWalle_Laurens/Sessie5/strawberry1.tif --i2=/home/laurens/Documents/Laurens/2018_beeldinterpretatie_VandeWalle_Laurens/Sessie5/strawberry1.tif
