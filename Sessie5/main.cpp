c431d8377b18a902a1fef6cadb4017146bfa64d4
laurensvdwalle <laurens.vandewalle@student.kuleuven.be>
2018-12-05 17:29:46 +0100

    KNN toegevoegd en getraint, geeft lege mask

--- Sessie5/main.cpp
+++ Sessie5/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/ml.hpp>
 
 #define GOED 0
 #define SLECHT 1
 
 using namespace std;
 using namespace cv;
+using namespace cv::ml;
 
-char window_name[20] = "Aarbeien";
+char window_name[20] = "Aardbeien";
 vector<Point> punten[2];
 int welkePunten;
 
 static void onMouse(int event, int x, int y, int flags, void* userdata);
+Ptr<KNearest> trainKNN(Mat trainingDataTot, Mat labels, int K);
+void Masks(Mat hsvImage, Ptr<KNearest> kclassifier);
 
 int main(int argc, const char** argv)
 {
@@ -83,7 +87,48 @@ int main(int argc, const char** argv)
     imshow(window_name, img);
     waitKey(0);
 
-    // descriptor bepalen op basis van de punten in HSV
+    // image converteren naar hsv
+    Mat hsvImage;
+    cvtColor(img,hsvImage, CV_BGR2HSV);
+    GaussianBlur( hsvImage, hsvImage, Size( 5, 5 ), 0, 0 );
+
+    // goede waarden inlezen
+    Mat trainingDataGoed(punten[GOED].size(), 3, CV_32FC1);     // trainingdata, met 3 waarden: h, s en v
+    Mat labelsGoed(punten[GOED].size(), 1, CV_32SC1);           // labels, met 1 waarden: GOED of SLECHT
+
+    for( unsigned i=0 ; i<punten[GOED].size() ; i++)
+    {
+        Vec3b descriptor = hsvImage.at<Vec3b>(punten[GOED][i].y, punten[GOED][i].x);
+        trainingDataGoed.at<float>(i,0) = descriptor[0];
+        trainingDataGoed.at<float>(i,1) = descriptor[1];
+        trainingDataGoed.at<float>(i,2) = descriptor[2];
+        labelsGoed.at<float>(i) = GOED;
+    }
+
+    // slecht waarden inlezen
+    Mat trainingDataSlecht(punten[SLECHT].size(), 3, CV_32FC1); // trainingdata, met 3 waarden: h, s en v
+    Mat labelsSlecht(punten[SLECHT].size(), 1, CV_32SC1);       // labels, met 1 waarden: GOED of SLECHT
+
+    for( unsigned i=0 ; i<punten[SLECHT].size() ; i++)
+    {
+        Vec3b descriptor = hsvImage.at<Vec3b>(punten[SLECHT][i].y, punten[SLECHT][i].x);
+        trainingDataSlecht.at<float>(i,0) = descriptor[0];
+        trainingDataSlecht.at<float>(i,1) = descriptor[1];
+        trainingDataSlecht.at<float>(i,2) = descriptor[2];
+        labelsSlecht.at<float>(i) = SLECHT;
+    }
+
+    // samenvoegen goede en slechte trainingdata
+    Mat trainingDataTot, labels;
+    vconcat(trainingDataGoed, trainingDataSlecht, trainingDataTot);
+    vconcat(labelsGoed, labelsSlecht, labels);
+
+    // KNN trainen
+    Ptr<KNearest> kclassifier;
+    kclassifier = trainKNN(trainingDataTot, labels, 3);
+
+    // Masks aanmaken
+    Masks(hsvImage, kclassifier);
 
     return 0;
 }
@@ -124,53 +169,57 @@ static void onMouse(int event, int x, int y, int flags, void* userdata)
     {
         cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
         cout << "lijst van " << puntchar << endl;
-        for(int punt = 0 ; punt < punten[welkePunten].size() ; punt++)
+        for(unsigned punt = 0 ; punt < punten[welkePunten].size() ; punt++)
         {
             cout << "Punt op (x=" << punten[welkePunten][punt].x << " y=" << punten[welkePunten][punt].y << ")" << endl;
         }
     }
 }
 
-void trainKNN(Mat img)
+Ptr<KNearest> trainKNN(Mat trainingDataTot, Mat labels, int K)
 {
-    int K = 3;
+    // KNN aanmaken
+    Ptr<TrainData> trainingData;
+    Ptr<KNearest> kclassifier=KNearest::create();
 
-    // image converteren naar hsv
-    Mat hsvImage;
-    cvtColor(img,hsvImage, CV_BGR2HSV);
+    trainingData=TrainData::create(trainingDataTot, SampleTypes::ROW_SAMPLE, labels);
 
-    // goede waarden inlezen
-    Mat trainingDataGoed(punten[GOED].size(), 3, CV_32FC1);     // trainingdata, met 3 waarden: h, s en v
-    Mat labelsGoed(punten[GOED].size(), 1, CV_32SC1);           // labels, met 1 waarden: GOED of SLECHT
+    kclassifier->setIsClassifier(true);
+    kclassifier->setAlgorithmType(KNearest::Types::BRUTE_FORCE);
+    kclassifier->setDefaultK(K);
 
-    for( int i=0 ; i<punten[GOED].size() ; i++)
+    // KNN trainen
+    kclassifier->train(trainingData);
+
+    return kclassifier;
+}
+
+void Masks(Mat hsvImage, Ptr<KNearest> kclassifier)
+{
+    // masks aanmaken
+    Mat KNNMask = Mat::zeros(hsvImage.rows, hsvImage.cols, CV_8UC1);
+    Mat KNNLabels;
+
+    for( int i=0 ; i<hsvImage.rows ; i++)
     {
-        Vec3b descriptor = hsvImage.at<Vec3b>(punten[GOED][i].y, punten[GOED][i].x);
-        trainingDataGoed.at<float>(i,0) = descriptor[0];
-        trainingDataGoed.at<float>(i,1) = descriptor[1];
-        trainingDataGoed.at<float>(i,2) = descriptor[2];
-        labelsGoed.at<float>(i) = GOED;
+        for( int j=0 ; j<hsvImage.cols ; j++)
+        {
+            Vec3b pixel = hsvImage.at<Vec3b>(i, j);
+            Mat hsvPixel(1, 3, CV_32FC1);
+
+            hsvPixel.at<float>(0, 0) = pixel[0];
+            hsvPixel.at<float>(0, 1) = pixel[1];
+            hsvPixel.at<float>(0, 2) = pixel[2];
+
+            kclassifier->findNearest(hsvPixel, kclassifier->getDefaultK(), KNNLabels);
+
+            KNNMask.at<uchar>(i, j) = KNNLabels.at<float>(0, 0);
+        }
     }
 
-    // slecht waarden inlezen
-    Mat trainingDataSlecht(punten[SLECHT].size(), 3, CV_32FC1); // trainingdata, met 3 waarden: h, s en v
-    Mat labelsSlecht(punten[SLECHT].size(), 1, CV_32SC1);       // labels, met 1 waarden: GOED of SLECHT
+    KNNMask = KNNMask*255;
 
-    for( int i=0 ; i<punten[SLECHT].size() ; i++)
-    {
-        Vec3b descriptor = hsvImage.at<Vec3b>(punten[SLECHT][i].y, punten[SLECHT][i].x);
-        trainingDataSlecht.at<float>(i,0) = descriptor[0];
-        trainingDataSlecht.at<float>(i,1) = descriptor[1];
-        trainingDataSlecht.at<float>(i,2) = descriptor[2];
-        labelsSlecht.at<float>(i) = SLECHT;
-    }
-
-    // samenvoegen goede en slechte trainingdata
-    Mat trainingData, labels;
-    vconcat(trainingDataGoed, trainingDataSlecht, trainingData);
-    vconcat(labelsGoed, labelsSlecht, labels);
-
-    // CvKNearest knn( trainingData, labels, 0, false, K );
-    // CvMat* nearests = cvCreateMat( 1, K, CV_32FC1);
+    imshow("KNN mask", KNNMask);
+    waitKey(0);
 }
 //--i1=/home/laurens/Documents/Laurens/2018_beeldinterpretatie_VandeWalle_Laurens/Sessie5/strawberry1.tif --i2=/home/laurens/Documents/Laurens/2018_beeldinterpretatie_VandeWalle_Laurens/Sessie5/strawberry1.tif
