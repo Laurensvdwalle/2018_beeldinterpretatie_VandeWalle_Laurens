6505c54d43ed6f3526013d2d77dcc6e4cdf00c20
LaurensVdW <laurens.vandewalle@student.kuleuven.be>
2018-12-12 17:26:46 +0100

    Sessie6 bijna afgewerkt

--- Sessie6/Project1/main.cpp
+++ Sessie6/Project1/main.cpp
@@ -7,6 +7,7 @@ using namespace cv;
 char window_name[20] = "faces";
 
 void violaJones(Mat frame);
+void LBP(Mat frame);
 
 int main(int argc, const char** argv)
 {
@@ -39,6 +40,7 @@ int main(int argc, const char** argv)
         throw "Error when reading steam_avi";
 
     namedWindow( window_name, 1);
+
     while( true )
     {
         capture >> videoInFrame;
@@ -47,6 +49,7 @@ int main(int argc, const char** argv)
             break;
 
         violaJones(videoInFrame);
+        LBP(videoInFrame);
 
         waitKey(20);
     }
@@ -75,6 +78,32 @@ void violaJones(Mat frame)
         Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
         ellipse( frame, center, Size( faces[i].width*0.7, faces[i].height*0.7), 0, 0, 360, Scalar( 255, 255, 255 ), 4, 8, 0 );
         putText(frame, std::to_string(level_weights[i]), Point(faces[i].x+10, faces[i].y+10), 1, 1, Scalar(0,255,0));
+    }
+
+    imshow(window_name, frame);
+}
+
+void LBP(Mat frame)
+{
+    // convert frame to gray values
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, CV_BGR2GRAY );
+
+    // load face cascade
+    CascadeClassifier face_cascade;
+    face_cascade.load( "lbpcascade_frontalface_improved.xml" );
+
+    // detect faces
+    std::vector<Rect> faces;
+    std::vector<int> level_weights;
+    face_cascade.detectMultiScale( frame_gray, faces, level_weights);
+
+    // draw circles on faces
+    for( unsigned i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+        ellipse( frame, center, Size( faces[i].width*0.7, faces[i].height*0.7), 0, 0, 360, Scalar( 255, 255, 255 ), 4, 8, 0 );
+        putText(frame, std::to_string(level_weights[i]), Point(faces[i].x+10, faces[i].y+10), 1, 1, Scalar(255,0,0));
     }
 
     imshow(window_name, frame);
